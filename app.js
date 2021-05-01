ec67e10dcbc9089dafea3573c5ec9515bc9283fa
tarmkalavan <tk.kalavan@gmail.com>
2021-05-01 14:14:54 +0700

    Add error handling

--- app.js
+++ app.js
@@ -89,3 +89,20 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/topics', topicRouter);
+
+//handling errors
+
+app.use((req,res,next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        }
+    });
+});
